cd603cda9dcdb1f76676da35eb17f8d84b7d1266
MANO-B <maikegam@ncc.go.jp>
2025-04-14 17:04:45 +0900

    added epsilon

--- src/deepcolor/funcs.py
+++ src/deepcolor/funcs.py
@@ -5,17 +5,15 @@ def calc_kld(qz):
     kld = -0.5 * (1 + qz.scale.pow(2).log() - qz.loc.pow(2) - qz.scale.pow(2))
     return(kld)
 
-
 def calc_poisson_loss(ld, norm_mat, obs):
-    p_z = dist.Poisson(ld * norm_mat + 1.0e-10)
-    l = - p_z.log_prob(obs)
-    return(l)
-        
-    
+    ld = ld * norm_mat
+    p_z = dist.Poisson(ld + 1.0e-10)
+    l = - p_z.log_prob(obs).clamp(min=-10)
+    return l
+
 def calc_nb_loss(ld, norm_mat, theta, obs):
     ld = norm_mat * ld
-    p =  ld / (ld + theta)
+    p = ld / (ld + theta)
     p_z = dist.NegativeBinomial(theta, p)
-    l = - p_z.log_prob(obs)
-    return(l)
-        
+    l = - p_z.log_prob(obs).clamp(min=-10)
+    return l        
