8c7a0732515c557afb94a0a458ec4ad723694312
MANO-B <maikegam@ncc.go.jp>
2025-05-26 23:16:17 +0900

    Update setup.py

--- src/deepcolor/setup.py
+++ src/deepcolor/setup.py
@@ -1,3 +1,8 @@
-from setuptools import setup
+from setuptools import setup, find_package
 
-setup()
+setup(
+    name='deepcolor',
+    version='0.1.0',
+    packages=find_packages(),
+    include_package_data=True
+)
